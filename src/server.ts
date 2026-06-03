e99ab9b51da5a4c9477d63f74dd56b2b73906fbb
Your Name <you@example.com>
2026-06-03 16:42:11 -0300

    server

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+const server = Bun.serve({
+    port: 3000,
+    routes: {
+        "/api-debugger": Bun.file('public/api-debugger.html'),
+        "/test": {
+            GET: () => Response.json({ time: Date.now() }),
+            PUT: () => Response.json({ time: Date.now() }),
+            POST: () => Response.json({ time: Date.now() }),
+            DELETE: () => Response.json({ time: Date.now() })
+        }
+    },
+    fetch() {
+        return new Response("Not Found", { status: 404 })
+    }
+})
+
+console.log(`⚡ http://localhost:${server.port}`) 
